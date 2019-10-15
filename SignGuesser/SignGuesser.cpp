83a5fecdd8b51ac5dd198ce3f089cd503b5881bb
Vincent Thomassin-Rochon <vincent.rochon8@gmail.com>
2019-10-15 14:22:14 -0400

    added qt base

--- SignGuesser/SignGuesser.cpp
+++ SignGuesser/SignGuesser.cpp
@@ -1,1 +1,11 @@
 #include "SignGuesser.h"
+#include <QApplication>
+
+int main(int argc, char* argv[])
+{
+	QApplication EditorApp(argc, argv);
+	Notepad Editor;
+	Editor.show();
+
+	return EditorApp.exec();
+}
